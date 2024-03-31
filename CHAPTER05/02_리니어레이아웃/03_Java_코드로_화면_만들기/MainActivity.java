4bd97bc5f18164384f36d95acea0216561d88e30
among5094 <106166621+among5094@users.noreply.github.com>
2024-04-01 00:27:56 +0900

    Add files via upload

--- CHAPTER05/02_리니어레이아웃/03_Java_코드로_화면_만들기/MainActivity.java
+++ CHAPTER05/02_리니어레이아웃/03_Java_코드로_화면_만들기/MainActivity.java
@@ -1,14 +1,12 @@
-package com.example.miniproject05_03;
+package com.example.ex05_08;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,37 +26,25 @@ public class MainActivity extends AppCompatActivity {
         LinearLayout baseLayout = new LinearLayout(this); //this: MainActivity의 현재 인스턴스를 참조
         baseLayout.setOrientation(LinearLayout.VERTICAL); //android:orientation="vertical"
         baseLayout.setBackgroundColor(Color.rgb(0,255,0)); //android:background="#00FF00"
-        
+
         //두 객체를 화면에 출력
         setContentView(baseLayout, params);
         //setContentView(View view, ViewGroup.LayoutParams params)이라서 두 객체의 위치가 바뀌면 오류임.
         //baseLayout은 뷰 그 자체이고, params는 뷰의 레이아웃 파라미터를 정의하는 객체이기 때문
-        
-        //에디트텍스트
-        EditText editText = new EditText(this);
-        editText.setText("IT Cookbook. Android");
-        editText.setTextColor(Color.BLACK);
-        editText.setVisibility(View.INVISIBLE); //보이지 않게 설정했다가
-        
-        //텍스트뷰 생성
-        TextView textView = new TextView(this);
-        textView.setText("IT Cookbook. Android");
-        textView.setTextColor(Color.MAGENTA);
-        
+
         //버튼 객체 생성
         Button btn = new Button(this); //btn객체는 MainActivity의 현재 인스턴스를 참조
         btn.setText("버튼입니다."); //btn에 글자 설정
-        btn.setBackgroundColor(Color.YELLOW); //btn에 색상 지정
+        btn.setBackgroundColor(Color.MAGENTA); //btn에 색상 지정
 
-        //객체를 레이아웃에 출력하기
-        baseLayout.addView(editText);
-        baseLayout.addView(textView);
+        //버튼을 레이아웃에 출력하기
         baseLayout.addView(btn);
 
         //버튼을 클릭했을 때 동작하는 리스너
         btn.setOnClickListener(new View.OnClickListener(){
             public void onClick(View arg0){
-                editText.setVisibility(View.VISIBLE); //버튼이 눌리면 에디트텍스트가 보이게 설정
+                Toast.makeText(getApplicationContext(),
+                        "자바 코드로 생성한 버튼입니다.", Toast.LENGTH_SHORT).show();
             }
         });
     }
