8497f9a01cb178ce69865a946a3392357ea1f418
among5094 <106166621+among5094@users.noreply.github.com>
2024-04-05 23:41:07 +0900

    Add files via upload

--- CHAPTER06/01_고급_위젯/MainActivity.java
+++ CHAPTER06/01_고급_위젯/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.miniproject06_01;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
@@ -50,6 +53,8 @@ public class MainActivity extends AppCompatActivity {
         tPicker.setVisibility(View.INVISIBLE);
         calView.setVisibility(View.INVISIBLE);
 
+        
+        /*라디오 버튼을 클릭했을 때의 이벤트 리스너*/
         //라디오 버튼(날짜 설정 라디오 버튼) 클릭 시 캘린더 뷰만 보이게 클래스 작성
         rdoCal.setOnClickListener(new View.OnClickListener(){
             public void onClick(View V){
@@ -66,17 +71,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //예약 시작을 클릭하면 크로노미터가 시작되도록 클릭 리스너 작성
+        btnStart.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                chrono.setBase(SystemClock.elapsedRealtime()); //크로노미터가 현재 시간부터 시간을 측정하기 시작하도록 설정(크로노미터 초기화)
+                chrono.start();
+                chrono.setTextColor(Color.RED); //시작을 알리기 위한 red
+            }
+        });
+
+        //예약 완료를 클릭하면 정지되도록 클릭 리스너 작성
+        btnEnd.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                chrono.stop();
+                chrono.setTextColor(Color.BLUE);
+
+            }
+        });
+
+        //사용자가 캘린더 뷰에서 날짜를 선택할 때마다 선택된 연도, 월, 일이 저장됨
+        calView.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth){
+                selectYear=year;
+                selectMonth=month+1; //1월부터 시작하니까 1더해주기
+                selectDay=dayOfMonth;
+
+            }
+        });
+
+        // 텍스트 뷰에 표시하기
+        //int year, int month, int dayOfMonth 이기 때문에 전부 int로 선언되어서, 텍스트 뷰에 표시하려면 toString해야됨
+        tvYear.setText(Integer.toString(selectYear));
+        tvMonth.setText(Integer.toString(selectMonth));
+        tvDay.setText(Integer.toString(selectDay));
+
+        tvHour.setText(Integer.toString(tPicker.getCurrentHour()));
+        tvMinute.setText(Integer.toString(tPicker.getCurrentMinute()));
+
 
     }
 }
-
-
-
-
-
-
-
-
-
-
 
